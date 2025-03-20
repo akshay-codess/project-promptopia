c0d8a18e3453a09e187ea737c94dde018475426b
Akshay Singh <akshayy.codes@gmail.com>
2025-03-20 22:59:06 +0530

    made progress on general profile page

--- app/profile/[profileId]/page.jsx
+++ app/profile/[profileId]/page.jsx
@@ -1,7 +1,18 @@
+import Profile from "@components/Profile"
+
 // TODO: Implement the ProfilePage component
 const ProfilePage = () => {
+
+    // TODO: fetch name desc data later
+
     return (
-        <div>ProfilePage </div>
+        <Profile
+            name={"My"}
+            desc="Welcome to your personalized profile page"
+            data={[]}
+            handleEdit={() => { }}
+            handleDelete={() => { }}
+        />
     )
 }
 
